9bca14fbf26f5a9a495dd8d932d2c8c235cf3b2b
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-09-10 00:06:26 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
     // origin:'http://localhost:5173', 
-    origin:'https://jobify-jobseeker.netlify.app/',
+    origin:'https://jobify-jobseeker.netlify.app',
     credentials:true 
 }
 app.use(cors(corsOptions));
