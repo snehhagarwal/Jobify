54bbe23277981aa26f6fcb21bb8112f53cc3a621
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-09-09 23:27:11 +0530

    updated package.json

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,7 +4,7 @@
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
 
 
-export const USER_API_END_POINT="https://jobify-production-b490.up.railway.app/api/v1/user";
-export const JOB_API_END_POINT="https://jobify-production-b490.up.railway.app/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobify-production-b490.up.railway.app/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobify-production-b490.up.railway.app/api/v1/company";
+export const USER_API_END_POINT="https://jobify-jobseekerbackend.up.railway.app/api/v1/user";
+export const JOB_API_END_POINT="https://jobify-jobseekerbackend.up.railway.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobify-jobseekerbackend.up.railway.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://jobify-jobseekerbackend.up.railway.app/api/v1/company";
