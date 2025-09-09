a573cc8b076db1d282221d9c3e740a6d88d7e4ca
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-09-09 23:44:55 +0530

    updated index,js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,6 @@ import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 const app=express();
 
-//middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
 app.use(cookieParser());
